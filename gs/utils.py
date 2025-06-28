8ee1f674eceab117cbb12a97aaee7431fd488077
takah29 <takah0209@gmail.com>
2025-06-29 01:00:19 +0900

    クオータニオンとして読み込むようにした

--- gs/utils.py
+++ gs/utils.py
@@ -9,8 +9,8 @@ def load_colmap_data(base_path: Path) -> dict[str, npt.NDArray]:
     reconstruction = pycolmap.Reconstruction(str(base_path))
     points_3d = np.vstack([pt.xyz for pt in reconstruction.points3D.values()], dtype=np.float32)
 
-    rot_mat_batch = np.stack(
-        [img.cam_from_world.rotation.matrix().T for img in reconstruction.images.values()],
+    quat_batch = np.stack(
+        [img.cam_from_world.rotation.quat for img in reconstruction.images.values()],
         dtype=np.float32,
     )
     t_vec_batch = np.stack(
@@ -30,7 +30,7 @@ def load_colmap_data(base_path: Path) -> dict[str, npt.NDArray]:
     )
     return {
         "points_3d": points_3d,  # (x, y, z) in points_3d
-        "rot_mat_batch": rot_mat_batch,  # 3x3 rotation_matrix in rot_mat_batch
+        "quat_batch": quat_batch,  # 3x3 rotation_matrix in rot_mat_batch
         "t_vec_batch": t_vec_batch,  # (tx, ty, tz) in t_vec_batch
         "intrinsic_batch": intrinsic_batch,  # (fx, fy, cx, cy) in intrinsic_batch
     }
