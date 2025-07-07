b61fb211efe09e58c16edad7ac2fb21bd3918958
takah29 <takah0209@gmail.com>
2025-07-08 07:36:58 +0900

    バグ修正

--- gs/loss_function.py
+++ gs/loss_function.py
@@ -21,7 +21,7 @@ def dssim_loss(output: jax.Array, target: jax.Array, window_size: int = 11) -> j
 
 def _create_2d_gaussian_kernel(window_size: int, sigma: float = 1.5) -> jax.Array:
     half_size = window_size // 2
-    kernel_1d = jnp.exp(jnp.arange(-half_size, half_size + 1) ** 2 / (2 * sigma**2))
+    kernel_1d = jnp.exp(-(jnp.arange(-half_size, half_size + 1) ** 2) / (2 * sigma**2))
     kernel = kernel_1d[:, None] @ kernel_1d[None, :]
     kernel = kernel / kernel.sum()
     return jnp.stack([kernel] * 3, axis=2)  # (window_size, window_size, 3)
@@ -29,16 +29,16 @@ def _create_2d_gaussian_kernel(window_size: int, sigma: float = 1.5) -> jax.Arra
 
 def _ssim(img1: jax.Array, img2: jax.Array, window_size: int) -> jax.Array:
     kernel = _create_2d_gaussian_kernel(window_size)
-    mu1 = convolve(img1, kernel, mode="valid")
-    mu2 = convolve(img2, kernel, mode="valid")
+    mu1 = convolve(img1, kernel, mode="same")
+    mu2 = convolve(img2, kernel, mode="same")
 
     mu1_sq = jnp.square(mu1)
     mu2_sq = jnp.square(mu2)
     mu1_mu2 = mu1 * mu2
 
-    sigma1_sq = convolve(jnp.square(img1), kernel, mode="valid") - mu1_sq
-    sigma2_sq = convolve(jnp.square(img2), kernel, mode="valid") - mu2_sq
-    sigma12 = convolve(img1 * img2, kernel, mode="valid") - mu1_mu2
+    sigma1_sq = convolve(jnp.square(img1), kernel, mode="same") - mu1_sq
+    sigma2_sq = convolve(jnp.square(img2), kernel, mode="same") - mu2_sq
+    sigma12 = convolve(img1 * img2, kernel, mode="same") - mu1_mu2
 
     c1 = 0.01**2
     c2 = 0.03**2
