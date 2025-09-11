af324950ee048d6fda53879e818f276136efa8e4
takah29 <takah0209@gmail.com>
2025-09-11 22:30:49 +0900

    Snugboxの実装

--- gs/core/tiling_and_sorting.py
+++ gs/core/tiling_and_sorting.py
@@ -18,6 +18,60 @@ def analytical_max_eigenvalue(mat2x2: jax.Array) -> jax.Array:
     return (trace + sqrt_term) * 0.5
 
 
+def _compute_index_intervals_naive(gaussians: dict[str, jax.Array], tile_size: int) -> jax.Array:
+    """99%信頼区間によるタイル範囲の計算"""
+    # ガウシアンの所属するタイルのインデックスを計算
+    gauss_max_eigvals = jax.vmap(analytical_max_eigenvalue)(gaussians["covs_2d"])
+    r_batch = 3.0 * jnp.sqrt(gauss_max_eigvals)[:, None]
+    gaussian_intervals = jnp.stack(
+        (gaussians["means_2d"] - r_batch, gaussians["means_2d"] + r_batch), axis=1
+    )
+    gaussian_index_intervals = (gaussian_intervals // tile_size).astype(
+        jnp.int32
+    )  # [[x_low_idx, y_low_idx], [x_high_idx, y_high_idx]]
+    return gaussian_index_intervals
+
+
+def _compute_snugbox_bounds(
+    mean_2d: jax.Array, cov_2d_inv_flat: jax.Array, opacity: jax.Array
+) -> jax.Array:
+    mu_x, mu_y = mean_2d[0], mean_2d[1]
+    a, b, c = cov_2d_inv_flat
+    t = 2 * jnp.log(255.0 * jnp.maximum(opacity, 1e-6))
+
+    minus_det = b * b - a * c
+    x_ = jnp.sqrt(-(b**2) * t / (minus_det * a))
+    x_args = jnp.array([x_, -x_])
+
+    sqrt_d = jnp.sqrt(minus_det * x_args + t * c)
+    y_cand = jnp.hstack(((-b * x_args + sqrt_d) / c + mu_y, (-b * x_args - sqrt_d) / c + mu_y))
+    y_min = y_cand.min()
+    y_max = y_cand.max()
+
+    y_ = jnp.sqrt(-(b**2) * t / (minus_det * c))
+    y_args = jnp.array([y_, -y_])
+
+    sqrt_d = jnp.sqrt(minus_det * y_args + t * a)
+    x_cand = jnp.hstack(((-b * y_args + sqrt_d) / a + mu_x, (-b * y_args - sqrt_d) / a + mu_x))
+    x_min = x_cand.min()
+    x_max = x_cand.max()
+    return jnp.array([[x_min, y_min], [x_max, y_max]])
+
+
+def _compute_index_intervals_snugbox(gaussians: dict[str, jax.Array], tile_size: int) -> jax.Array:
+    """SnugBoxによるタイル範囲の計算
+
+    Reference: https://speedysplat.github.io/
+    """
+    gaussian_intervals = jax.vmap(_compute_snugbox_bounds)(
+        gaussians["means_2d"], gaussians["covs_2d_inv_flat"], gaussians["opacities"]
+    )
+    gaussian_index_intervals = (gaussian_intervals // tile_size).astype(
+        jnp.int32
+    )  # [[x_low_idx, y_low_idx], [x_high_idx, y_high_idx]]
+    return gaussian_index_intervals
+
+
 def _depth_sorted_indices(
     tile_index: jax.Array,
     gaussian_index_intervals: jax.Array,
@@ -41,14 +95,8 @@ def _create_tile_depth_decending_indices_batch(
     tile_index_batch: jax.Array,
     consts: dict[str, Any],
 ) -> jax.Array:
-    # ガウシアンの所属するタイルのインデックスを計算
-    gauss_max_eigvals = jax.vmap(analytical_max_eigenvalue)(gaussians["covs_2d"])
-    r_batch = 3.0 * jnp.sqrt(gauss_max_eigvals)[:, None]
-    gaussian_intervals = jnp.stack(
-        (gaussians["means_2d"] - r_batch, gaussians["means_2d"] + r_batch), axis=1
-    )
-    gaussian_index_intervals = (gaussian_intervals // consts["tile_size"]).astype(
-        jnp.int32
+    gaussian_index_intervals = _compute_index_intervals_snugbox(
+        gaussians, consts["tile_size"]
     )  # [[x_low_idx, y_low_idx], [x_high_idx, y_high_idx]]
 
     return jax.lax.map(
